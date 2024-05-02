1af5baf7329cb77487cb8104a7292615a61718cb
ctoloczko <celina.toloczko@gmail.com>
2024-05-02 12:54:08 +0200

    reverted to initial ideal of travelling asistant after fixing value issue in js

--- src/script.js
+++ src/script.js
@@ -7,7 +7,7 @@ function displayTrip(response) {
   new Typewriter("#destination", {
     strings: response.data.answer,
     autoStart: true,
-    delay: 50,
+    delay: 20,
     cursor: "",
     pauseFor: 1500000,
   });
@@ -19,7 +19,7 @@ function findMyTrip(event) {
   let userInput = document.querySelector("#user-input").value;
   let apiKey = "8eatdeae3d0b8e63a64512c0d2f3a54o";
   let context =
-    "You are an experienced AI travel assistant who has traveled all around the world and spend lots of time exploring the world. You need to suggest the destinationn for the next trip considering user preferences provided in user's instructions. Pleas ebe polite and provide only one option and include a 4 lines description";
+    "You are an experienced AI travel assistant who has traveled all around the world and spend lots of time exploring the world. You need to suggest the destinationn for the next trip considering user preferences provided in user's instructions. Please be polite and provide only one option and include a short candid description of maximal 3 lines. Please sign the recommendation at the bottom separated with <br/> element and inside <strong> element with Your Coddiwomple Team powered by SheCodes AI";
   let prompt = `User' instructions: ${userInput}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
